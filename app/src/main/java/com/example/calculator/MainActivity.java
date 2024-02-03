296705442667bb4cfe06e1b5d7485c2a498b0397
MOHAMED <aboelftohm1@gmail.com>
2024-02-03 21:45:14 +0200

    init commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.calculator;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button dot ;
+    TextView historyNum ;
+    String savedValueNumber ="";
+    String savedOperator ="";
+    boolean onEqualClick = false ;
+    TextView result  ;
+    boolean clickOperator = false ;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result = findViewById(R.id.textResult) ;
+        dot = findViewById(R.id.dot) ;
+        historyNum = findViewById(R.id.history);
+    }
+    @SuppressLint("SuspiciousIndentation")
+    public void onClickNumber(View view){
+        historyNum.append(((Button) view).getText().toString());
+        if(onEqualClick)
+        {
+            result.setText(((Button) view).getText().toString());
+            if(((Button) view).getText().toString().equals(".")) {
+                view.setEnabled(false);
+            }
+            onEqualClick = false ;
+        }
+        else {
+            result.append(((Button) view).getText().toString());
+            if(((Button) view).getText().toString().equals("."))
+                view.setEnabled(false);
+        }
+    }
+    public void onClickOperation(View view){
+        if(result.getText().toString().isEmpty())
+            return;
+
+        if(savedOperator.isEmpty())
+        {
+            savedValueNumber = result.getText().toString();
+        }else {
+            String rhs = result.getText().toString();
+            savedValueNumber = calculate(savedValueNumber,savedOperator,rhs);
+        }
+        savedOperator = ((Button) view).getText().toString();
+        result.setText("");
+        historyNum.append(((Button) view).getText().toString());
+        clickOperator = true ;
+        dot.setEnabled(true);
+    }
+    public void clickEqual(View view)
+    {
+        if(savedValueNumber.equals("")) {
+           return;
+        }
+        if(!result.getText().toString().equals("") && !clickOperator) {
+            dot.setEnabled(true);
+            result.setText(result.getText().toString());
+            onEqualClick = true;
+        }
+        else{
+            dot.setEnabled(true);
+            String RHS = result.getText().toString();
+            savedValueNumber = calculate(savedValueNumber, savedOperator, RHS);
+            result.setText(savedValueNumber);
+            savedValueNumber = "";
+            savedOperator = "";
+            onEqualClick = true;
+            clickOperator = false ;
+        }
+        historyNum.setText(savedValueNumber);
+    }
+
+    public String calculate(String lhs , String op , String rhs)
+    {
+        double num1 = Double.parseDouble(lhs);
+        double num2 = Double.parseDouble(rhs);
+        double res = 0 ;
+        if(op.equals("+"))
+        {
+            res = num1 + num2 ;
+        } else if (op.equals("-")) {
+            res = num1 - num2 ;
+        }
+        else if (op.equals("/")) {
+            res = num1 / num2 ;
+        }
+        else if (op.equals("*")) {
+            res = num1 * num2 ;
+        }
+        return ""+res;
+    }
+}
